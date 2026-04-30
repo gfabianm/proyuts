08ecd96ffa293e698576c892ddc83212b082c7c2
Fabian Gelvez <fabiangelvez04@gmail.com>
2026-04-29 20:35:56 -0500

    Base de datos actualizada

---
+++ bd/proyuts_db_V2.sql
@@ -0,0 +1,405 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 30-04-2026 a las 03:33:10
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `proyuts_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `id` bigint(20) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `descripcion` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`id`, `nombre`, `descripcion`) VALUES
+(1, 'Desarrollo Empresarial', 'Cursos orientados a aplicaciones empresariales'),
+(2, 'Nuevas Tecnologias', 'Cursos sobre tendencias y herramientas modernas'),
+(3, 'Programacion Web', 'Cursos de desarrollo web'),
+(4, 'Aplicaciones Moviles', 'Cursos de apps móviles'),
+(5, 'POO', 'Programación orientada a objetos');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cursos`
+--
+
+CREATE TABLE `cursos` (
+  `id` bigint(20) NOT NULL,
+  `nombre` varchar(150) NOT NULL,
+  `descripcion` text DEFAULT NULL,
+  `cupo_maximo` int(11) NOT NULL DEFAULT 30,
+  `proyuts_otorgados` int(11) NOT NULL DEFAULT 0,
+  `categoria_id` bigint(20) NOT NULL,
+  `estado_id` bigint(20) NOT NULL,
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp(),
+  `profesor_id` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `cursos`
+--
+
+INSERT INTO `cursos` (`id`, `nombre`, `descripcion`, `cupo_maximo`, `proyuts_otorgados`, `categoria_id`, `estado_id`, `fecha_creacion`, `profesor_id`) VALUES
+(1, 'Programacion Web', 'Curso orientado al desarrollo de aplicaciones web', 30, 10, 3, 1, '2026-04-22 03:58:46', NULL),
+(2, 'Aplicaciones Moviles', 'Curso enfocado en desarrollo móvil', 25, 12, 4, 1, '2026-04-22 03:58:46', NULL),
+(3, 'POO', 'Curso de programación orientada a objetos', 35, 8, 5, 1, '2026-04-22 03:58:46', NULL),
+(5, 'curos prueba', 'dasdas', 3, 2, 3, 1, '2026-04-22 04:17:00', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `curso_profesores`
+--
+
+CREATE TABLE `curso_profesores` (
+  `curso_id` bigint(20) NOT NULL,
+  `profesor_id` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estados_curso`
+--
+
+CREATE TABLE `estados_curso` (
+  `id` bigint(20) NOT NULL,
+  `nombre` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `estados_curso`
+--
+
+INSERT INTO `estados_curso` (`id`, `nombre`) VALUES
+(5, 'CANCELADO'),
+(1, 'DISPONIBLE'),
+(3, 'EN_CURSO'),
+(4, 'FINALIZADO'),
+(2, 'LLENO');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `historial_proyuts`
+--
+
+CREATE TABLE `historial_proyuts` (
+  `id` bigint(20) NOT NULL,
+  `usuario_id` bigint(20) NOT NULL,
+  `total_acumulado` int(11) NOT NULL,
+  `fecha_corte` timestamp NOT NULL DEFAULT current_timestamp()
+) ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `inscripciones`
+--
+
+CREATE TABLE `inscripciones` (
+  `id` bigint(20) NOT NULL,
+  `usuario_id` bigint(20) NOT NULL,
+  `curso_id` bigint(20) NOT NULL,
+  `fecha_inscripcion` timestamp NOT NULL DEFAULT current_timestamp(),
+  `estado_inscripcion` varchar(30) NOT NULL DEFAULT 'INSCRITO'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `inscripciones`
+--
+
+INSERT INTO `inscripciones` (`id`, `usuario_id`, `curso_id`, `fecha_inscripcion`, `estado_inscripcion`) VALUES
+(9, 2, 1, '2026-04-28 04:36:09', 'INSCRITO');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proyuts`
+--
+
+CREATE TABLE `proyuts` (
+  `id` bigint(20) NOT NULL,
+  `usuario_id` bigint(20) NOT NULL,
+  `curso_id` bigint(20) DEFAULT NULL,
+  `cantidad` int(11) NOT NULL,
+  `descripcion` varchar(255) DEFAULT NULL,
+  `fecha_registro` timestamp NOT NULL DEFAULT current_timestamp(),
+  `semestre` varchar(20) DEFAULT NULL,
+  `anio` int(11) DEFAULT NULL
+) ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` bigint(20) NOT NULL,
+  `nombre` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`id`, `nombre`) VALUES
+(1, 'ADMINISTRADOR'),
+(3, 'ESTUDIANTE'),
+(2, 'PROFESOR');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` bigint(20) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `apellido` varchar(100) NOT NULL,
+  `email` varchar(120) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `programa_academico` varchar(120) DEFAULT NULL,
+  `biografia` varchar(500) DEFAULT NULL,
+  `foto` varchar(255) DEFAULT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT 1,
+  `fecha_registro` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `email`, `password`, `telefono`, `programa_academico`, `biografia`, `foto`, `activo`, `fecha_registro`) VALUES
+(1, 'Fabian', 'Gelvez', 'admin@proyuts.com', '$2a$10$b3nQSilm3kTM8WZt/hVPVePx1Gen5UYugeqB0doKWVguyXcZWxKrK', '3000000000', 'Ingenieria de Software', 'Usuario de prueba', 'default.png', 1, '2026-04-22 03:00:00'),
+(2, 'Jhonatan', 'Gelvez', 'Jhonatan@proyuts.edu.co', '$2a$10$LbO2BWC0ElfTeHCjPDhaoOYTF7.EX1Uq.C36XQai8Zzw5XPNIAkUm', '3124567890', 'Sistemas', 'Soy un nuevo estudiante', 'default.png', 1, '2026-04-28 02:52:12');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario_roles`
+--
+
+CREATE TABLE `usuario_roles` (
+  `usuario_id` bigint(20) NOT NULL,
+  `rol_id` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuario_roles`
+--
+
+INSERT INTO `usuario_roles` (`usuario_id`, `rol_id`) VALUES
+(1, 1),
+(2, 3);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nombre` (`nombre`);
+
+--
+-- Indices de la tabla `cursos`
+--
+ALTER TABLE `cursos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_cursos_categoria` (`categoria_id`),
+  ADD KEY `fk_cursos_estado` (`estado_id`),
+  ADD KEY `fk_cursos_profesor` (`profesor_id`);
+
+--
+-- Indices de la tabla `curso_profesores`
+--
+ALTER TABLE `curso_profesores`
+  ADD PRIMARY KEY (`curso_id`,`profesor_id`),
+  ADD KEY `fk_curso_profesores_profesor` (`profesor_id`);
+
+--
+-- Indices de la tabla `estados_curso`
+--
+ALTER TABLE `estados_curso`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nombre` (`nombre`);
+
+--
+-- Indices de la tabla `historial_proyuts`
+--
+ALTER TABLE `historial_proyuts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_historial_usuario` (`usuario_id`);
+
+--
+-- Indices de la tabla `inscripciones`
+--
+ALTER TABLE `inscripciones`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uk_usuario_curso` (`usuario_id`,`curso_id`),
+  ADD KEY `fk_inscripciones_curso` (`curso_id`);
+
+--
+-- Indices de la tabla `proyuts`
+--
+ALTER TABLE `proyuts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_proyuts_usuario` (`usuario_id`),
+  ADD KEY `fk_proyuts_curso` (`curso_id`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nombre` (`nombre`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indices de la tabla `usuario_roles`
+--
+ALTER TABLE `usuario_roles`
+  ADD PRIMARY KEY (`usuario_id`,`rol_id`),
+  ADD KEY `fk_usuario_roles_rol` (`rol_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `cursos`
+--
+ALTER TABLE `cursos`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `estados_curso`
+--
+ALTER TABLE `estados_curso`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `historial_proyuts`
+--
+ALTER TABLE `historial_proyuts`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `inscripciones`
+--
+ALTER TABLE `inscripciones`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de la tabla `proyuts`
+--
+ALTER TABLE `proyuts`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `cursos`
+--
+ALTER TABLE `cursos`
+  ADD CONSTRAINT `fk_cursos_categoria` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_cursos_estado` FOREIGN KEY (`estado_id`) REFERENCES `estados_curso` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_cursos_profesor` FOREIGN KEY (`profesor_id`) REFERENCES `usuarios` (`id`);
+
+--
+-- Filtros para la tabla `curso_profesores`
+--
+ALTER TABLE `curso_profesores`
+  ADD CONSTRAINT `fk_curso_profesores_curso` FOREIGN KEY (`curso_id`) REFERENCES `cursos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_curso_profesores_profesor` FOREIGN KEY (`profesor_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `historial_proyuts`
+--
+ALTER TABLE `historial_proyuts`
+  ADD CONSTRAINT `fk_historial_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `inscripciones`
+--
+ALTER TABLE `inscripciones`
+  ADD CONSTRAINT `fk_inscripciones_curso` FOREIGN KEY (`curso_id`) REFERENCES `cursos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_inscripciones_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `proyuts`
+--
+ALTER TABLE `proyuts`
+  ADD CONSTRAINT `fk_proyuts_curso` FOREIGN KEY (`curso_id`) REFERENCES `cursos` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_proyuts_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `usuario_roles`
+--
+ALTER TABLE `usuario_roles`
+  ADD CONSTRAINT `fk_usuario_roles_rol` FOREIGN KEY (`rol_id`) REFERENCES `roles` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_usuario_roles_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
